dda05cff6b5785870274a42fb7be86b11cb195da
ghourimarti <ghourimarti@gmail.com>
2025-03-17 21:39:53 +0000

    adding initial layout of python project

--- git.sh
+++ git.sh
@@ -1,9 +1,18 @@
-
+echo
 echo "<------- status check ------>"
+
+echo
 git status
+
+echo
 echo "<------- git add ------>"
+echo
 git add *
+echo
 echo "<------- git commit ------>"
+echo
 git commit -m "adding initial layout of python project"
+echo
 echo "<------- git push ------>"
+echo
 git push
